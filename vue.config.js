9ec9f8d569a0aff74765208a1b395eb069f4e988
Normone <46660709+Normone@users.noreply.github.com>
2021-04-15 21:12:52 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/tamagochi/'
+  //   : '/'
+  
+}
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tamagochi/'
+    : '/'
 }
