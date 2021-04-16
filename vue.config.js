985403187a26e91a6712ee9b8246d3675aa2b0ca
Normone <vashhekin0d0@gmail.com>
2021-04-17 00:15:22 +0700

    Удаление лишнего

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,10 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ],
-  // publicPath: process.env.NODE_ENV === 'production'
-  //   ? '/tamagochi/'
-  //   : '/'
-  
+  ]  
 }
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/tamagochi/'
-    : '/'
-}
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/tamagochi/'
+//     : '/'
+// }
